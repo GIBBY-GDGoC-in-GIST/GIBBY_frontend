389526325939fa413977a7d61714b76cf903b78f
bkindtoevery1 <chayhyeon@gmail.com>
2025-04-06 17:39:46 +0900

    Fix jest.config.js and package.json

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
     moduleNameMapper: {
-      '^react-router-dom$': '<rootDir>/node_modules/react-router-dom',
-      '^react-router-dom/(.*)$': '<rootDir>/node_modules/react-router-dom/$1'
+      '^react-router-dom$': require.resolve('react-router-dom'),
+      '^react-router-dom/(.*)$': require.resolve('react-router-dom/$1')
     },
     transform: {
       '^.+\\.(js|jsx)$': 'babel-jest'
